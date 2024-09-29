e3ed23bf571b3fd3c375c1c84f2f002ba105a309
Thiago Marciel F Silva <th1ago_m@hotmail.com>
2024-09-29 19:28:03 -0300

    chore(client): update method name

--- src/application/use-cases/client/GetClient.ts
+++ src/application/use-cases/client/GetClient.ts
@@ -4,7 +4,7 @@ export class GetClient {
   constructor(private clientRepository: ClientRepository) {}
 
   async execute(id: string) {
-    const client = await this.clientRepository.get(id)
+    const client = await this.clientRepository.getById(id)
     if (!client) throw new Error('Client not found')
     return client
   }
